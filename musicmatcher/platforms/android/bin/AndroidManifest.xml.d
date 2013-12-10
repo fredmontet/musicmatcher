356089d96a39c790bb68ced22caab26541d8306b
FlavienKnuchel <knuchelf@gmail.com>
2013-12-10 09:05:23 +0100

    test

--- musicmatcher/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/media/www-dev/public/musicmatcher/musicmatcher/platforms/android/bin/AndroidManifest.xml : \
-/media/www-dev/public/musicmatcher/musicmatcher/platforms/android/AndroidManifest.xml \
--- musicmatcher/platforms/android/bin/MusicMatcher-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/media/www-dev/public/musicmatcher/musicmatcher/platforms/android/bin/MusicMatcher-debug-unaligned.apk : \
-/media/www-dev/public/musicmatcher/musicmatcher/platforms/android/bin/MusicMatcher.ap_ \
-/media/www-dev/public/musicmatcher/musicmatcher/platforms/android/bin/classes.dex \
-/media/www-dev/public/musicmatcher/musicmatcher/platforms/android/libs/cordova-3.2.0.jar \
