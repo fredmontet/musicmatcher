1b62b5382f4218db970899d5fcea0961f5afbaa3
FlavienKnuchel <knuchelf@gmail.com>
2013-12-10 10:02:04 +0100

    organisation des fonctions

--- musicmatcher/www/js/index.js
+++ musicmatcher/www/js/index.js
@@ -1,20 +1,4 @@
-$(window).load(function() {
-    $(document).scrollTop($('#ball').offset().top);  
-});
 
-$(document).ready(function(){
-    $('#home').hide();
-    $('#home').fadeIn(500);
-    document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
-});
-
-function addsong(){
-    $(document).scrollTop($('#add3').offset().top);
-}
-
-function radar(){
-    $(document).scrollTop($('#radar').offset().top);
-}
 /*------------------------------------- DO NO TOUCH -------------------------------------*/
 
 /*var app = {
