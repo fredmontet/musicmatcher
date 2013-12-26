909e127e0c6cef5ce7c246805b9f7c0ce69890c0
fredmontet <fredmontet@gmail.com>
2013-12-26 19:46:30 +0100

    commit de merge 2

--- musicmatcher/www/js/lookupsong.js
+++ musicmatcher/www/js/lookupsong.js
@@ -1,10 +1,49 @@
 /*
-*   Fichier allant chercher les infos de spotify pour peupler les champs de recherche
-*/
+ *   Fichier allant chercher les infos de spotify pour peupler les champs de recherche
+ */
 
+// Quand le DOM est charg�
+$(document).ready(function(){
+
+    // Quand on appuie sur une touche du champ artiste
+    $("#artist").on("keyup", function(){
+        
+        var artistName = $("#artist").val();   
+        
+        $.getJSON("http://ws.spotify.com/search/1/track?q=artist:"+artistName+'&callback=?', {
+            }, function(reponseAJAX){
+                if(reponseAJAX.num_results > 0){
+                    // Cr�er une nouvelle liste
+                    $("#selectSong").html('<select id="songs"></select>');
+                    // pour chaque sons pr�sents
+                    $.each(reponseAJAX.tracks, function(value) {
+                        // cr�e une option
+                        var $currentOption = $("<option>");
+					
+                        // Met le texte de l'option
+                        $currentOption.html(value.name);
+                        
+                        // ajoute l'option � la liste des sons
+                        $("#artistes").append($currentOption);
+
+					
+                    }); // Each
+    
+                }// if
+                else {
+                    // supprime la liste
+                    $("#selectArtist").html('');
+                }// else
+            }); // getJSON
+    }); // #search on key up
+   
+});
+
+
+    /**
 $(document).ready(function(){
     
-    $("#artist").keyup(catch_artist($("#artist")));  
+    $("#songname").keyup(catch_artist($("#artist")));  
     
     
 });
@@ -22,3 +61,18 @@ $(document).ready(function(){
         
       console.log("fin catch_artist");
     }
+        
+      console.log("start Artist!")
+      //var artist = $(this).artist;
+      var artist = $('#artist');
+     $.getJSON("http://ws.spotify.com/search/1/track?q=artist:"+artist, 
+     
+      function (data){   
+          alert(data)
+          console.log(artist)
+          console.log(data)
+          console.log("end Artist!")
+      })
+  }
+     */
+
