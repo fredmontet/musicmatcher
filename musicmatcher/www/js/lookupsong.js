df0bae618cb5c7197bde85552d9638073189eb7f
fredmontet <fredmontet@gmail.com>
2013-12-26 12:01:54 +0100

    ajout d'un fichier lookupsong

---
+++ musicmatcher/www/js/lookupsong.js
@@ -0,0 +1,13 @@
+/*
+*   Fichier allant chercher les infos de spotify pour peupler les champs de recherche
+*/
+
+$(document).ready(function(){
+    
+   function catch_artist{
+       
+   }
+    
+    
+    
+})
