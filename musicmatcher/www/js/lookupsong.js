d533bbb7d44495de7d8eb63ceb1349b1d794513d
Louis Allemann <louis.allemann@heig-vd.ch>
2013-12-27 12:37:08 +0100

    lookUP

--- musicmatcher/www/js/lookupsong.js
+++ musicmatcher/www/js/lookupsong.js
@@ -3,8 +3,8 @@
  */
 
 /*
-*   Fonction qui va chercher les titres des musiques en fonction d'un artiste
-*/
+ *   Fonction qui va chercher les titres des musiques en fonction d'un artiste
+ */
 function catch_artist(){
     console.log("debut catch_artist");
 
@@ -23,6 +23,7 @@ function catch_artist(){
                 $addOption.html(track.name);
                 // ajoute le nom de l'artiste et le nom du sons à l'option
                 $addOption.data("artisteName", data.name);
+                $addOption.data("trackName", data.name);
                 // ajoute l'option à la liste des sons
                 $("#songs").append($addOption);
                     
@@ -32,19 +33,33 @@ function catch_artist(){
                 //ajoute les options avec un titre
                 addOption(track.name);                   
             })//each
-            
-            youtubeSearch($("#songs option:selected").data('artisteName'));
-        });//getJSON 
-                
-    console.log("fin catch_artist");   
+            // Par défaut on charge les articles le premier élément de la liste #songs
+            var $premiereOption = $("#songs").find("option").first();
+            youtubeSearch( $premiereOption.data("artisteName") , $premiereOption.data("trackName") );
+            // evenement sur changement de la liste des sons
+            $("#songs").on("change", function(event) {
+                youtubeSearch( $("#songs option:selected").data('artisteName'), $("#songs option:selected").data('trackName') );
+            });//getJSON 
+            console.log(artisteName)
+            console.log("fin catch_artist");  
+        });
 } //catch_artist
-            
+    
+    
+function youtubeSearch(artisteName, trackName){
+    console.log("debut youtbeSearch");
+    
+    $.getJSON("https://gdata.youtube.com/feeds/api/videos?q="+artisteName+"+"+trackName+"&max-results=1&v=2", {
+        }, function(data){    
+     
+        });
+}        
 
-/*
-* Fonction qui peuple la drop down list des titres de musique
-*/
-//function addOption(track_name){
-   // var option = $("<option/>");
-   // option.text(track_name);
-  //  $("#songs").append(option);
-//}
+    /*
+     * Fonction qui peuple la drop down list des titres de musique
+     */
+    //function addOption(track_name){
+    // var option = $("<option/>");
+    // option.text(track_name);
+    //  $("#songs").append(option);
+    //}
