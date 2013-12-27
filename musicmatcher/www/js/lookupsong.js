99924e40c585758e0022246ffb98ce010b0a5b17
Louis Allemann <louis.allemann@heig-vd.ch>
2013-12-27 12:50:54 +0100

    lookpup yt

--- musicmatcher/www/js/lookupsong.js
+++ musicmatcher/www/js/lookupsong.js
@@ -11,38 +11,39 @@ function catch_artist(){
     var artistName = $("#artist").val();   
         
     $.getJSON("http://ws.spotify.com/search/1/track.json?q=artist:"+artistName, {
-        }, function(data){    
-            // Creer une nouvelle liste
-            $("#selectSong").html('<select id="songs"></select>');
+        }, function(data){   
+                // Creer une nouvelle liste
+                $("#selectSong").html('<select id="songs"></select>');
                     
-            //Pour chaque track
-            $.each(data.tracks, function(i, track){
-                // crée une option
-                var $addOption = $("<option>");
-                // Met le texte de l'option
-                $addOption.html(track.name);
-                // ajoute le nom de l'artiste et le nom du sons à l'option
-                $addOption.data("artisteName", data.name);
-                $addOption.data("trackName", data.name);
-                // ajoute l'option à la liste des sons
-                $("#songs").append($addOption);
+                //Pour chaque track
+                $.each(data.tracks, function(i, track){
+                    // crée une option
+                    var $addOption = $("<option>");
+                    // Met le texte de l'option
+                    $addOption.html(track.name);
+                    // ajoute le nom de l'artiste et le nom du sons à l'option
+                    $addOption.data("artisteName", data.name);
+                    $addOption.data("trackName", data.name);
+                    // ajoute l'option à la liste des sons
+                    $("#songs").append($addOption);
                     
-                //test console
-                console.log(track.name);
+                    //test console
+                    console.log(track.name);
                     
-                //ajoute les options avec un titre
-                addOption(track.name);                   
-            })//each
-            // Par défaut on charge les articles le premier élément de la liste #songs
-            var $premiereOption = $("#songs").find("option").first();
-            youtubeSearch( $premiereOption.data("artisteName") , $premiereOption.data("trackName") );
-            // evenement sur changement de la liste des sons
-            $("#songs").on("change", function(event) {
-                youtubeSearch( $("#songs option:selected").data('artisteName'), $("#songs option:selected").data('trackName') );
-            });//getJSON 
-            console.log(artisteName)
+                    //ajoute les options avec un titre
+                    addOption(track.name);                   
+                })//each
+                // Par défaut on charge les articles le premier élément de la liste #songs
+                var $premiereOption = $("#songs").find("option").first();
+                youtubeSearch( $premiereOption.data("artisteName") , $premiereOption.data("trackName") );
+                // evenement sur changement de la liste des sons
+                $("#songs").on("change", function(event) {
+                    youtubeSearch( $("#songs option:selected").data('artisteName'), $("#songs option:selected").data('trackName') );
+                });//getJSON 
+
             console.log("fin catch_artist");  
         });
+        
 } //catch_artist
     
     
@@ -50,7 +51,7 @@ function youtubeSearch(artisteName, trackName){
     console.log("debut youtbeSearch");
     
     $.getJSON("https://gdata.youtube.com/feeds/api/videos?q="+artisteName+"+"+trackName+"&max-results=1&v=2", {
-        }, function(data){    
+        }, function(data){  
      
         });
 }        
